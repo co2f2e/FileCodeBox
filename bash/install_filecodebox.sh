19290fe72dcc0e559af81524e7441c0c822abe6f
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-04-08 01:51:28 +0800

    Update install_filecodebox.sh

--- bash/install_filecodebox.sh
+++ bash/install_filecodebox.sh
@@ -18,11 +18,18 @@ else
     exit 0
 fi
 
+cd /usr/local/FileCodeBox || { echo "无法进入 FileCodeBox 目录"; exit 0; }
+
 if [ -f "$FILE_PATH" ]; then
     cp "$FILE_PATH" "${FILE_PATH}.bak"
     sed -i "s/0.0.0.0/127.0.0.1/g" "$FILE_PATH"
 else
-    echo "$FILE_PATH 文件不存在，跳过修改。"
+    echo "$FILE_PATH 文件不存在，脚本终止。"
+    echo 0
+fi
+
+if ! dpkg -l | grep -q python3-venv; then
+    sudo apt-get install -y python3-venv
 fi
 
 if [ ! -d "$VENV_DIR" ]; then
@@ -30,7 +37,7 @@ if [ ! -d "$VENV_DIR" ]; then
     echo "虚拟环境已创建：$VENV_DIR"
 fi
 
-source "$VENV_DIR/bin/activate"
+source "$VENV_DIR/bin/activate" || { echo "无法激活虚拟环境"; exit 0; }
 
 if [ -f "requirements.txt" ]; then
     pip install --upgrade pip 

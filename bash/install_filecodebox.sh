de03109ea1380cc1a3b0b505e1373581a2e17286
co2f2e <48397609+co2f2e@users.noreply.github.com>
2025-04-10 23:10:59 +0800

    Update install_filecodebox.sh

--- bash/install_filecodebox.sh
+++ bash/install_filecodebox.sh
@@ -1,12 +1,34 @@
 #!/bin/bash
 clear
+export LANG="en_US.UTF-8"
+red='\033[0;31m'
+green='\033[0;32m'
+yellow='\033[0;33m'
+blue='\033[0;36m'
+nc='\033[0m'
+
+red() {
+	echo -e "${red}$1${nc}"
+}
+
+green() {
+	echo -e "${green}$1${nc}"
+}
+
+yellow() {
+	echo -e "${yellow}$1${nc}"
+}
+
+blue() {
+	echo -e "${blue}$1${nc}"
+}
 
 FILE_PATH="/usr/local/FileCodeBox/main.py"
 VENV_DIR="/usr/local/FileCodeBox/venv"
 SERVICE_NAME="filecodebox"
 
 if ! command -v git &>/dev/null; then
-    echo "Git 未安装，正在安装 Git..."
+    yellow "Git 未安装，正在安装 Git..."
     sudo apt update
     sudo apt install -y git
 fi
@@ -15,17 +37,17 @@ cd /usr/local
 if [ ! -d "FileCodeBox" ]; then
     git clone https://github.com/vastsa/FileCodeBox.git
 else
-    echo "FileCodeBox 仓库已存在，脚本终止。"
+    red "FileCodeBox 仓库已存在，脚本终止。"
     exit 0
 fi
 
-cd /usr/local/FileCodeBox || { echo "无法进入 FileCodeBox 目录"; exit 0; }
+cd /usr/local/FileCodeBox || { red "无法进入 FileCodeBox 目录"; exit 0; }
 
 if [ -f "$FILE_PATH" ]; then
     cp "$FILE_PATH" "${FILE_PATH}.bak"
     sed -i "s/0.0.0.0/127.0.0.1/g" "$FILE_PATH"
 else
-    echo "$FILE_PATH 文件不存在，脚本终止。"
+    red "$FILE_PATH 文件不存在，脚本终止。"
     exit 0
 fi
 
@@ -35,16 +57,16 @@ fi
 
 if [ ! -d "$VENV_DIR" ]; then
     python3 -m venv "$VENV_DIR"
-    echo "虚拟环境已创建：$VENV_DIR"
+    green "虚拟环境已创建：$VENV_DIR"
 fi
 
-source "$VENV_DIR/bin/activate" || { echo "无法激活虚拟环境"; exit 0; }
+source "$VENV_DIR/bin/activate" || { red "无法激活虚拟环境"; exit 0; }
 if [ -f "requirements.txt" ]; then
     pip install --upgrade pip
     pip install -r requirements.txt
-    echo "依赖包安装完成。"
+    green "依赖包安装完成。"
 else
-    echo "未找到 requirements.txt 文件，脚本终止。"
+    red "未找到 requirements.txt 文件，脚本终止。"
     exit 0
 fi
 
@@ -79,10 +101,9 @@ Environment=PROJECT_DIR=/usr/local/FileCodeBox
 WantedBy=multi-user.target
 EOF
 
-echo "已将服务添加为 Systemd 服务并设置为开机自启。"
+green "已将服务添加为 Systemd 服务并设置为开机自启。"
 
 sudo systemctl daemon-reload
 sudo systemctl start $SERVICE_NAME.service
 sudo systemctl enable $SERVICE_NAME.service
 sudo systemctl status $SERVICE_NAME.service
-
